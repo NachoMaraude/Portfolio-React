8fc2be2e38c42ad2aff0388618bdf74b0cdb93c7
NachoMaraude <maraudenacho@gmail.com>
2022-12-14 01:28:11 -0300

    render

--- client/src/redux/actions/nodemailerAction.js
+++ client/src/redux/actions/nodemailerAction.js
@@ -4,6 +4,20 @@ import axios from "axios";
 export const postEmail = createAsyncThunk(
     "email/postEmail",
     async (payload) => {
-        return await axios.post("http://localhost:3001/api/email", payload);
+        try {
+            if (window.location.href.slice(0, 16) !== "http://localhost") {
+                return await axios.post(
+                    "https://portfolio-backend-cefs.onrender.com/api/email",
+                    payload
+                );
+            } else {
+                return await axios.post(
+                    "http://localhost:3001/api/email",
+                    payload
+                );
+            }
+        } catch (error) {
+            console.log("error -----------> ", error);
+        }
     }
 );
